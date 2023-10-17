eab7164c6c93c44f41a87a06ea1a0c669a8a9aa9
beqa-dogonadze <~beqadogonadze328@gmail.com>
2023-10-17 23:08:46 +0400

    dd

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,11 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className='flex justify-center items-center gap-2 px-7 py-4 border rounded-full font-montserrat text-lg leading-none bg-coral-red text-white border-teal-200'>
       {label}
-      <img
+      {iconURL && <img
         src={iconURL}
         alt='arrow right icon'
         className='ml-2 rounded-full w-5 h-5'
-      />
+      />}
     </button>
   )
 }
--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,8 +1,17 @@
-const PopularProductCard = ({ imgURL, name, price }) => {
+import { star } from '../assets/icons'
+const PopularProductCard = ({ imgURL, name, price, rate }) => {
   return (
-    <div className="flex flex-1 flex-col w-full max-sm:w-full">
-      
+    <div className='flex flex-1 flex-col w-full max-sm:w-full'>
+      <img src={imgURL} alt={name} className='w-[280px] h-[280px]' />
+      <div className='mt-8 flex justify-start gap-2.5'>
+        <img src={star} alt='rating' width={24} height={24} />
+        <p className='font-montserat text-xl leading-normal text-slate-gray'>
+          {rate}
+        </p>
+      </div>
+      <h3 className='mt-2 text-xl leading-normal font-samibold font-palanquin'>{name}</h3>
+      <p className='mt-2 font-semibold font-montserrat text-coral-red leading-normal text-2xl'>{price}</p>
     </div>
-    )
+  )
 }
 export default PopularProductCard
